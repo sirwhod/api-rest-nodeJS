d92b56045c670ab515fb39c82a2962b7b60f36cf
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-03-31 18:10:23 -0300

    class: Criando aplicação

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'hello NodeJS!'
+})
+
+app.listen({
+  port: 3333,
+}).then((port) => {
+  console.log(`server listening on ${port}`)
+})
