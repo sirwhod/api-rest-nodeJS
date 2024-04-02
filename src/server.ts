842200b061acfc66fd08f46b645d08c769e62c55
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-01 22:22:02 -0300

    class: Plugins do Fastfy

--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,11 @@ import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const transaction = knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
