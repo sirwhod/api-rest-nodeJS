dd7a45767dc4d03f2225f531936769870169515b
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-02 21:25:52 -0300

    class: Criando primeiro teste

---
+++ test/exemple.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+  // fazer a chamada http
+
+  // o que eu espero?
+
+  const responseStatusCode = 201
+
+  expect(responseStatusCode).toEqual(201)
+})
